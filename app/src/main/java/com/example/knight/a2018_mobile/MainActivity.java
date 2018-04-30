427ecb2c2057b7cc9bb573d30aca4eceff8397f1
Damotorie <parksjin01@gmail.com>
2018-04-30 23:19:55 +0900

    Add custom socket class which is used to communicate with web server

--- app/src/main/java/com/example/knight/a2018_mobile/MainActivity.java
+++ app/src/main/java/com/example/knight/a2018_mobile/MainActivity.java
@@ -17,94 +17,46 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.Date;
+import java.net.Socket;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
-    CheckBox chk1;
-    CheckBox chk2;
-    TextView status;
-    RadioGroup radio;
-    RadioButton radio1;
-    RadioButton radio2;
-    RadioButton radio3;
-
-    String n;
+    Button medicine_search_btn;
+    EditText medicine_name_edt;
+//    String Server_IP="192.168.0.122";
+//    private int Server_PORT=9999;
+    String Server_IP="118.36.9.247";
+    private int Server_PORT=6000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // 이 부분이 XML을 자바 객체로 변경해주는 부분
-        btn = (Button)findViewById(R.id.btn);
-        chk1 = (CheckBox)findViewById(R.id.chk1);
-        chk2 = (CheckBox)findViewById(R.id.chk2);
-        radio = (RadioGroup)findViewById(R.id.radio);
-        radio1 = (RadioButton)findViewById(R.id.radio1);
-        radio2 = (RadioButton)findViewById(R.id.radio2);
-        radio3 = (RadioButton)findViewById(R.id.radio3);
-//        status = (TextView)findViewById(R.id.status);
-        btn.setOnClickListener(new View.OnClickListener(){
 
-            public void onClick(View view){
-                n = "Coffee";
+        medicine_name_edt = (EditText) findViewById(R.id.medicine_name);
+        medicine_search_btn = (Button) findViewById(R.id.medicine_search_btn);
 
-                int id = radio.getCheckedRadioButtonId();
-                if(radio1.getId() == id){
-                    n = "Decaf " + n;
-                }
-                else if(radio2.getId() == id){
-                    n = "Espresso " + n;
-                }
-                else if(radio3.isSelected()){
-                    n = "Columbian " + n;
-                }
+        medicine_search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String medicine_name = medicine_name_edt.getText().toString();
 
-                if(chk1.isChecked())
-                {
-                    n += " & "+"Cream";
+                if(medicine_name.compareTo("") == 0){
+                    return;
+                }else {
+                    MySocket sock = new MySocket(Server_IP, Server_PORT);
+                    Toast.makeText(getApplicationContext(), sock.request(medicine_name), Toast.LENGTH_LONG).show();
                 }
-                if(chk2.isChecked()){
-                    n += " & "+"Sugar";
-                }
-
-                Toast.makeText(getApplicationContext(), n, Toast.LENGTH_LONG).show();
             }
         });
-
     }
-//    @Override
-//    protected void onStart() {
-//        super.onStart();
-//        Log.d(tag, "In the onStart() event");
-//
-//    }
-//    @Override
-//    protected void onStop() {
-//        super.onStop();
-//        Log.d(tag, "In the onStop() event");
-//    }
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        Log.d(tag, "In the onDestroy() event");
-//    }
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        Log.d(tag, "In the onPause() event");
-//    }
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        Log.d(tag, "In the onResume() event");
-//    }
-//    @Override
-//    protected void onRestart() {
-//        super.onRestart();
-//        Log.d(tag, "In the onRestart() event");
-//    }
 }
-
-
-// 네?
---
+++ app/src/main/java/com/example/knight/a2018_mobile/MySocket.java
@@ -0,0 +1,73 @@
+package com.example.knight.a2018_mobile;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by Knight on 2018. 4. 30..
+ *
+ * Socket class to communicate with web server
+ *
+ */
+
+public class MySocket extends Thread {
+    Socket sock;
+    BufferedReader networkReader;
+    BufferedWriter networkWriter;
+    PrintWriter out;
+    String Server_IP;
+    String request;
+    String res;
+    int Server_PORT;
+    int successive = 0;
+
+    /**
+     * @description Constructor of socket class
+     * @param new_Server_IP Server IP address
+     * @param new_Server_PORT Server port number
+     */
+    public MySocket(String new_Server_IP, int new_Server_PORT){
+        Server_IP = new_Server_IP;
+        Server_PORT = new_Server_PORT;
+    }
+
+    /**
+     * @description Send request to server and return result of request
+     * @param request Request to web server
+     * @return Result of request to web server
+     */
+    public String request(String request){
+        this.request = request;
+        this.start();
+        return res;
+    }
+
+    /**
+     *  @description Create socket, send request to server through socket and receive result with socket
+     */
+    public void run() {
+        try {
+            sock = new Socket(Server_IP, Server_PORT);
+            Log.d("Socket", "Passed");
+            networkReader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            networkWriter = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+            networkWriter.write(request);
+            networkWriter.flush();
+            res = networkReader.readLine();
+            Log.d("result", res);
+            successive = 1;
+        } catch (IOException e) {
+            Log.d("Socket", "Socket initialization is failed, " + e.getMessage());
+            successive = 0;
+        }
+    }
+}
