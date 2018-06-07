af5287c63366161148e13efdde38361631e9a390
leejisung <jisungnam@naver.com>
2018-06-07 20:59:23 +0900

    JunitTest add

--- app/src/test/java/com/example/knight/a2018_mobile/AlarmAdapterTest.java
+++ app/src/test/java/com/example/knight/a2018_mobile/AlarmAdapterTest.java
@@ -1,5 +1,8 @@
 package com.example.knight.a2018_mobile;
 
+import android.content.Context;
+import android.test.mock.MockContext;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,17 +12,33 @@ import static org.junit.Assert.*;
  */
 public class AlarmAdapterTest {
 
+    Context context = new MockContext();
+    AlarmAdapter test = new AlarmAdapter(context);
+
     @Test
     public void getCount() throws Exception {
+
+        int length;
+        length = test.getCount();
+        assertTrue(length > 0);
+        assertNotNull(length);
 
     }
 
     @Test
     public void getItem() throws Exception {
-    }
+        Object obj;
+        obj = test.getItem(0);
+        assertNotNull(obj);
+        obj = test.getItem(1);
+        assertNotNull(obj);
+        obj = test.getItem(2);
+        assertNotNull(obj);
+        obj = test.getItem(3);
+        assertNotNull(obj);
+        obj = test.getItem(4);
+        assertNotNull(obj);
 
-    @Test
-    public void getItemId() throws Exception {
     }
 
 }
